fe8fb099ac9df0c96da5eb515056e609bba04932
adikhoff <dikhoff@gmail.com>
2021-12-17 15:18:53 +0100

    Edge case

--- src/advent/Day17.java
+++ src/advent/Day17.java
@@ -31,14 +31,18 @@ public class Day17 {
             for (int vx = 1; vx <= targetx2; vx++) {
                 int ivx = vx; int ivy = vy;
                 int x = 0; int y = 0;
+                int shotHeight = 0;
                 while (x < targetx2 && y > targety1) {
                     x = x + ivx;
                     y = y + ivy;
-                    if (y > maxHeight) {
-                        maxHeight = y;
+                    if (y > shotHeight) {
+                        shotHeight = y;
                     }
                     if (x >= targetx1 && x <= targetx2 && y >= targety1 && y <= targety2) {
                         hits++;
+                        if (shotHeight > maxHeight) {
+                            maxHeight = shotHeight;
+                        }
                         break;
                     }
                     ivx = ivx - 1; if (ivx < 0) ivx = 0;
@@ -52,7 +56,7 @@ public class Day17 {
 
     public static void main(String[] args) throws IOException {
         Day17 day17 = new Day17();
-        List<String> lines = Files.lines(Paths.get("./data/day17.txt")).collect(Collectors.toList());
+        List<String> lines = Files.lines(Paths.get("./data/day17alter.txt")).collect(Collectors.toList());
         long start = System.currentTimeMillis();
         day17.day17(lines);
         System.out.printf("Done after %d millis%n", System.currentTimeMillis() - start);
