540d40851e7251a4843e6d2abfc47d84edd11e37
adikhoff <dikhoff@gmail.com>
2021-12-21 17:07:57 +0100

    Refactor

--- src/advent/Day21.java
+++ src/advent/Day21.java
@@ -3,6 +3,7 @@ package advent;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,18 +11,57 @@ import java.util.stream.Collectors;
 
 public class Day21 {
 
+    long[] wins = new long[] { 0, 0};
+    Map<Integer, Integer> rolls;
+
+
     public void solve(List<String> input) {
+        int[] pawns = input.stream().map(l -> l.split(":")[1].trim()).mapToInt(Integer::parseInt).toArray();
+
+        part1(Arrays.copyOf(pawns, 2));
+
+        this.rolls = diceRolls();
+        countAllWins(pawns, new int[] {0, 0}, 0, 1);
+        System.out.println("Part 2: " + Math.max(wins[0], wins[1]));
+    }
+
+    private void countAllWins(final int[] pawns, final int[] scores, final int p, final long occurs) {
+        for (var i = 0; i < 2; i++) {
+            if (scores[i] >= 21) {
+                this.wins[i] += occurs;
+                return;
+            }
+        }
+        this.rolls.forEach((die, roll) -> {
+            int[] ss = Arrays.copyOf(scores, 2);
+            int[] ps = Arrays.copyOf(pawns, 2);
+
+            ps[p] += die;
+            ps[p] %= 10;
+            if (ps[p] == 0) ps[p] = 10;
+            ss[p] += ps[p];
+            countAllWins(ps, ss, p == 0 ? 1 : 0, occurs * roll);
+        });
+    }
+
+    private Map<Integer, Integer> diceRolls() {
+        Map<Integer, Integer> rolls = new HashMap<>();
+        for (int die1 = 1; die1 <= 3; die1++) {
+            for (int die2 = 1; die2 <= 3; die2++) {
+                for (int die3 = 1; die3 <= 3; die3++) {
+                    rolls.merge(die1 + die2 + die3, 1, Integer::sum);
+                }
+            }
+        }
+        return rolls;
+    }
+
+    private void part1(int[] pawns) {
         int die = 1;
         int rolls = 0;
-        int[] pawns = new int[2];
         int[] scores = new int[2];
-        int player = 0;
+        int player;
         int winner;
-        for (String line : input) {
-            String[] parts = line.split(":");
-            pawns[player] = Integer.parseInt(parts[1].trim());
-            player++;
-        }
 
         finish:
         while (true) {
@@ -41,58 +81,8 @@ public class Day21 {
             }
         }
         System.out.println("Part 1: " + (scores[(winner + 1) % 2] * rolls));
-
-        for (String line : input) {
-            String[] parts = line.split(":");
-            pawns[player] = Integer.parseInt(parts[1].trim());
-            scores[player] = 0;
-            player++;
-        }
-        var diceRolls = diceRolls();
-        countAllWins(0, diceRolls, pawns[0], pawns[1], 0, 0, 0, 1);
-        System.out.println("Part 2: " + Math.max(win0, win1));
     }
 
-    long win0 = 0;
-    long win1 = 0;
-
-    private void countAllWins(final int step, Map<Integer, Integer> rolls, final int pawn0, final int pawn1,
-                              final int score0, final int score1, final int player, final long numOccurs) {
-        if (score0 >= 21) {
-            win0 += numOccurs;
-            return;
-        }
-        if (score1 >= 21) {
-            win1 += numOccurs;
-            return;
-        }
-        rolls.forEach((die, num) -> {
-            int newScore0 = score0, newScore1 = score1, newPawn0 = pawn0, newPawn1 = pawn1;
-            if (player == 0) {
-                newPawn0 += die;
-                if (newPawn0 > 10) newPawn0 = newPawn0 - 10;
-                newScore0 += newPawn0;
-            } else {
-                newPawn1 += die;
-                if (newPawn1 > 10) newPawn1 = newPawn1 - 10;
-                newScore1 += newPawn1;
-            }
-            countAllWins(step + 1, rolls, newPawn0, newPawn1, newScore0, newScore1,
-                    player == 0 ? 1 : 0, numOccurs * num);
-        });
-    }
-
-    private Map<Integer, Integer> diceRolls() {
-        Map<Integer, Integer> rolls = new HashMap<>();
-        for (int die1 = 1; die1 <= 3; die1++) {
-            for (int die2 = 1; die2 <= 3; die2++) {
-                for (int die3 = 1; die3 <= 3; die3++) {
-                    rolls.merge(die1 + die2 + die3, 1, Integer::sum);
-                }
-            }
-        }
-        return rolls;
-    }
 
     public static void main(String[] args) throws IOException {
         Day21 solver = new Day21();
